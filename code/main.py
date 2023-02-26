f268a7cf43dfbed58e62d781932ad49625714882
Schuler Henry Martin (BhP/HRL3.2-SH1) <scu8bh@bosch.com>
2023-02-26 13:58:47 +0100

    Added NN test for LPC and LPCC

--- code/main.py
+++ code/main.py
@@ -1,8 +1,10 @@
 from preprocessing.AudioPreprocessor import AudioPreprocessor
+from feature_extraction.LPCExtractor import LPCExtractor
 
 def main():
-    frames = AudioPreprocessor.load_preprocessed_frames("./audio.wav")
-    print(frames)
+    frames = AudioPreprocessor.load_preprocessed_frames("C:\\Users\\SCU8BH\\Documents\\T3000\\Studienarbeit\\Data\\50_speakers_audio_data\\Speaker_0003\\Speaker_0003_00000.wav")
+    lpccs = LPCExtractor.get_lpcc_from_frames(frames=frames, order=12)
+    print(len(lpccs))
     
 if __name__ == "__main__":
     main()
